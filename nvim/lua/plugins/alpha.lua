54782342edb12aaa491568ced88d110cc5567a96
Ulysses Kee <ulysseskcw96@gmail.com>
2023-02-13 01:00:04 +0800

    Add devicons to greetings

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -4,14 +4,16 @@ return {
     local current_time = os.time()
     local current_hour = tonumber(os.date("%H", current_time))
 
-    local greeting = "Good "
+    local greeting = ""
 
     if current_hour < 12 then
-      greeting = greeting .. "morning, Ulysses"
+      greeting = greeting .. "  Good morning, Ulysses"
     elseif current_hour < 18 then
-      greeting = greeting .. "afternoon, Ulysses"
+      greeting = greeting .. "  Good afternoon, Ulysses"
+    elseif current_hour < 21 then
+      greeting = greeting .. "  Good evening, Ulysses"
     else
-      greeting = greeting .. "night, Ulysses"
+      greeting = greeting .. "  Good night, Ulysses"
     end
 
     local logo = [[
@@ -22,7 +24,7 @@ return {
 	  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
 	  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
 
-                  %s
+                %s
     ]]
     logo = string.format(logo, greeting)
     opts.section.header.val = vim.split(logo, "\n", { trimempty = true })

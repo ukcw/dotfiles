588da7d8880d4c6e76287bc06d90632dba50f543
Ulysses Kee <ulysseskcw96@gmail.com>
2023-05-21 03:59:34 +0800

    feat: add fish configs

---
+++ fish/conf.d/abbr.fish
@@ -0,0 +1,7 @@
+abbr vim nvim
+abbr e nvim
+abbr mkdir "mkdir -p"
+
+# helpers to set Java version
+alias j18="export JAVA_HOME=`/usr/libexec/java_home -v 18`; java -version"
+alias j11="export JAVA_HOME=`/usr/libexec/java_home -v 11`; java -version"

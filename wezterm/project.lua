dcd1c099bb3cd48f9f6b94f088436d81112c3545
Ulysses Kee <ulysseskcw96@gmail.com>
2024-02-19 01:01:50 +0800

    reorg wezterm config

---
+++ wezterm/project.lua
@@ -0,0 +1,29 @@
+local startup = function(env_var, projects_module, wezterm)
+	local project = os.getenv(env_var)
+
+	if project == nil then
+		return
+	end
+
+	local status, project_module = pcall(function()
+		return require(projects_module .. "." .. project)
+	end)
+	if not status then
+		wezterm.log_error("Unable to import " .. project_module)
+		return
+	end
+	local project_startup = project_module.startup
+
+	if project_startup == nil or type(project_startup) ~= "function" then
+		wezterm.log_error(
+			"Project " .. project .. " has no exported 'startup' function (type is " .. type(project_startup) .. ")"
+		)
+		return
+	end
+
+	project_startup(wezterm)
+end
+
+return {
+	startup = startup,
+}
